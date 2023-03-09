1d99508d0e8b1f032c75d4c950cc5339573013b8
R-anil <126901051+R-anil@users.noreply.github.com>
2023-03-09 12:03:16 -0800

    Add files via upload

---
+++ sprites.py
@@ -0,0 +1,39 @@
+# File created by Ryan Anil
+
+import pygame as pg
+
+from pygame.sprite import Sprite
+
+from settings import *
+
+vec = pg.math.Vector2
+
+# create a player 
+
+class Player(Sprite):
+    def __init__(self):
+        Sprite.__init__(self)
+        self.image = pg.Surface((50,50))
+        self.image.fill(BLACK)
+        self.rect = self.image.get_rect()
+        self.pos = vec(WIDTH/2, HEIGHT/2)
+        self.vel = vec(0,0)
+        self.acc = vec(0,0)
+        self.cofric = 0.1
+        self.canjump = False
+    def input(self):
+        keystate = pg.key.get_pressed()
+        if keystate[pg.K_w]:
+            self.acc.y = -PLAYER_ACC
+        if keystate[pg.K_a]:
+            self.acc.x = -PLAYER_ACC
+        if keystate[pg.K_s]:
+            self.acc.y = PLAYER_ACC
+        if keystate[pg.K_d]:
+            self.acc.x = PLAYER_ACC
+    def update(self):
+        self.acc = self.vel * PLAYER_FRICTION
+        self.input()
+        self.vel += self.acc
+        self.pos += self.vel + 0.5 * self.acc
+        self.rect.center = self.pos
