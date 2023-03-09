3b4ee967d8b296b7b632f56857ef786f27b7cde6
R-anil <126901051+R-anil@users.noreply.github.com>
2023-03-09 12:03:01 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,14 @@
+# File created by Ryan Anil
+
+# screen dimensions
+WIDTH = 800
+HEIGHT = 600
+
+# game settings
+FPS = 30
+# player attributes
+PLAYER_ACC = 2
+PLAYER_FRICTION =-0.12
+# colors
+BLACK = (0,0,0)
+BLUE = (50,50,255)
