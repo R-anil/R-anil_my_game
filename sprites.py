18740d516365279a57d04a098279cd2fcec9e013
R-anil <126901051+R-anil@users.noreply.github.com>
2023-03-17 17:08:36 -0700

    Add files via upload

--- sprites.py
+++ sprites.py
@@ -53,30 +53,16 @@ class MOB(Sprite):
         self.image.fill(RED)
         self.rect = self.image.get_rect()
         self.pos = vec(WIDTH/2, HEIGHT/2)
-        self.vel = vec(0,0)
+        self.vel = vec(1,0)
         self.acc = vec(0,0)
         self.cofric = 0.1
         self.canjump = False
     def behavior(self):
-        # acc go up
-        self.acc.y = -MOB_ACC
-        # self.acc.x = -MOB_ACC
-        # self.acc.y = MOB_ACC
-        # self.acc.x = MOB_ACC
-        if self.rect.x > WIDTH:
-            print("I'm off the right screen...")
-        if self.rect.x < 0:
-            print("I'm off the left screen...")
-        if self.rect.y < 0:
-            print("I'm off the top screen...")
-            # reduces velocity
-            self.vel *= -1
-            self.pos.y <=0
-        if self.rect.y > HEIGHT:
-            print("I'm off the bottom screen...")
+        if self.rect.x > WIDTH or self.rect.x <0:
+            self.vel.x*= -1
+        if self.rect.y > HEIGHT or self.rect.y <0:
+            self.vel.y*= -1
     def update(self):
-        self.acc = self.vel * MOB_FRICTION
         self.behavior()
-        self.vel += self.acc
-        self.pos += self.vel + 0.5 * self.acc
+        self.pos+= self.vel     
         self.rect.center = self.pos
