0b5ba4eb5295d85bcce1a428a864a5d16e7615fc
R-anil <126901051+R-anil@users.noreply.github.com>
2023-03-24 13:48:39 -0700

    fixed broken code and added comments

--- main.py
+++ main.py
@@ -1,83 +1,93 @@
-# File created by Ryan Anil
+# File created by: Ryan Anil
 
-# import libs
+# testing changes with this line...more editssss....
+# asdfasdfasdfasdfasdf;skdjf;lkasjdf;lkasjd;flkjasdf;lk
+# ;ALSKJDF;LKASJDF;KLJASD;FLKJAS;DKLFJ
+# import libraries
+# test comment for git 
 import pygame as pg
 import random
 import os
-from random import randint
-# test comment for git hub
-# import settings 
+
+# need this to create file paths effectively
+from os import path
+
+# import settings
 from settings import *
 from sprites import *
+from random import randint
 # from pg.sprite import Sprite
 
 # set up assets folders
 game_folder = os.path.dirname(__file__)
-img_folder = os.path.join(game_folder, "img")
+img_folder = os.path.join(game_folder, "images")
 
-def get_mouse_now():
-    x,y = pg.mouse.get_pos()
-    return (x,y)
+# create the game class to organize game content better...
+class Game:
+    # initializes(inits) the pygame stuff including setting up screen/display area
+    def __init__(self):
+        pg.init()
+        pg.mixer.init()
+        self.screen = pg.display.set_mode((WIDTH, HEIGHT))
+        pg.display.set_caption("My Game...")
+        self.clock = pg.time.Clock()
+        self.running = True
+    # method to create a new game...
+    def new(self):
+            self.score = 0
+            self.all_sprites = pg.sprite.Group()
+            self.platforms = pg.sprite.Group()
+            self.enemies = pg.sprite.Group()
+            # instantiates player class from sprites file, and passes this game class as an arguement
+            self.player = Player(self)
+            self.all_sprites.add(self.player)
+            for i in range(1,10):
+                e = Mob()
+                self.all_sprites.add(e)
+            self.run()
+    def run(self):
+        self.playing = True
+        while self.playing:
+            self.clock.tick(FPS)
+            self.events()
+            self.update()
+            self.draw()
+    def events(self):
+        # Game Loop - events
+        for event in pg.event.get():
+            # check for closing window
+            if event.type == pg.QUIT:
+                if self.playing:
+                    self.playing = False
+                self.running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_SPACE:
+                    self.player.jump()
+    def draw_text(self, text, size, color, x, y):
+        font_name = pg.font.match_font('arial')
+        font = pg.font.Font(font_name, size)
+        text_surface = font.render(text, True, color)
+        text_rect = text_surface.get_rect()
+        text_rect.midtop = (x, y)
+        self.screen.blit(text_surface, text_rect)
+    # def get_mouse_now():
+    #     x,y = pg.mouse.get_pos()
+    #     return (x,y)
+    def update(self):
+        self.all_sprites.update()
+    def draw(self):
+        self.screen.fill(BLUE)
+        self.draw_text("Hello there!", 42, WHITE, WIDTH/2, HEIGHT/10)
+        self.all_sprites.draw(self.screen)
+        pg.display.flip()
 
-
-# init pg and create window
-pg.init()
-# init sound mixer
-pg.mixer.init()
-screen = pg.display.set_mode((WIDTH, HEIGHT))
-pg.display.set_caption("My first game...")
-clock = pg.time.Clock() 
-
-all_sprites = pg.sprite.Group()
-enemies = pg.sprite.Group()
-pewpews = pg.sprite.Group()
-
-# player is instantiated here
-player = Player()
-player.rect.x = 5
-invader = MOB()
-invader.image.fill((0,0,255))
-invader.vel = vec(randint(1,8),randint(1,8))
-
-for i in range (0,10):
-    m = MOB()
-    m.vel = invader.vel = vec(randint(10,50),randint(10,50))
-    all_sprites.add(m)
-    enemies.add(m)
-# testSprite = Sprite()
-# testSprite.image = pg.Surface((50,50))
-# testSprite.image.fill(GREEN)
-# testSprite.rect = testSprite.image.get_rect()
-# testSprite.rect.center = (WIDTH / 2, HEIGHT / 2)
-all_sprites.add(player)
-all_sprites.add(invader)
-# all_sprites.add(testSprite)
+vec = pg.math.Vector2
 
 # game loop
 
-while RUNNING:
-    #  keep loop running at the right speed
-    clock.tick(FPS)
-    ### process input events section of game loop
-    for event in pg.event.get():
-        # check for window closing
-        if event.type == pg.QUIT:
-            RUNNING = False
-            # break
-    # print(get_mouse_now())
-    ### update section of game loop (if updates take longer the 1/30th of a second, you will get laaaaag...)
-    all_sprites.update()
+g = Game()
 
-    blocks_hit_list = pg.sprite.spritecollide(player, enemies, True)
+while g.running:
+    g.new()
 
-    for block in blocks_hit_list:
-        print(enemies)
-        pass
-    ### draw and render section of game loop
-    screen.fill(BLUE)
-    all_sprites.draw(screen)
-    # double buffering draws frames for entire screen
-    pg.display.flip()
-    # pg.display.update() -> only updates a portion of the screen
-# ends program when loops evaluates to false
 pg.quit()
